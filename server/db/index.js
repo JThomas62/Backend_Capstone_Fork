4d92bf24cfea08e7254d108711925eb5d2a52607
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2023-11-11 11:44:47 -0500

    Update index.js
    Removed placeholders.  Set up client.

--- server/db/index.js
+++ server/db/index.js
@@ -8,7 +8,23 @@ const { Pool } = require("pg");
 // Pool allows interaction with Postgres Library; Info is no longer necessary to add here since it is in .env file
 const pool = new Pool();
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
 // export const query = (text, params) => pool.query(text, params);
+
+
+const { Client } = require("pg"); // Import pg module
+require("dotenv").config();
+
+const client = new Client({
+  connectionString: 
+    process.env.DATABASE_URL || 
+    "https://localhost:5432/bookclub",
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
+
+
+module.exports = {
+  client,
+};

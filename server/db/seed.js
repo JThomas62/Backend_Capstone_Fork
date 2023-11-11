047224cecbdee884c29e5d80684e73df7614f3a5
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2023-11-11 11:34:39 -0500

    Update seed.js, fixing indentation

--- server/db/seed.js
+++ server/db/seed.js
@@ -24,59 +24,59 @@ async function dropTables() {
 
 
 async function createTables() {
-    try {
-      console.log("Starting to build tables...");
-  
-      await client.query(`
-        CREATE TABLE books (
-          book_id SERIAL PRIMARY KEY,
-          title VARCHAR(100) NOT NULL,
-          author VARCHAR(100) NOT NULL,
-          rating DECIMAL(3,2),
-          description TEXT,
-          genre_id INT NOT NULL,
-          image_url VARCHAR(255),
-          active BOOLEAN,
-          CONSTRAINT fk_genre_id FOREIGN KEY(genre_id) REFERENCES genres(genre_id)
-        );
-        CREATE TABLE users (
-            user_id SERIAL PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
-            email VARCHAR(100) UNIQUE NOT NULL,
-            username VARCHAR(50) UNIQUE NOT NULL,
-            password VARCHAR(50) NOT NULL,
-            status VARCHAR(20)
-        );
-        CREATE TABLE comments (
-            comment_id SERIAL PRIMARY KEY,
-            user_id INT NOT NULL,
-            book_id INT NOT NULL,
-            comment TEXT NOT NULL,
-            rating DECIMAL(3,2),
-            CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
-            CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES books(book_id)
-        );
-        CREATE TABLE genres (
-            genre_id SERIAL PRIMARY KEY,
-            name VARCHAR(100) UNIQUE NOT NULL
-        );
-      `);
-      console.log("Finished building tables!");
-    } catch (error) {
-      console.error("Error building tables!");
-      throw error;
-    }
-  }
+  try {
+    console.log("Starting to build tables...");
 
-  async function rebuildDB() {
-    try {
-      client.connect();
-  
-      await dropTables();
-      await createTables();
-      // await createInitial Books/Users
-    } catch (error) {
-      console.log("Error during rebuildDB");
-      throw error;
-    }
+    await client.query(`
+      CREATE TABLE books (
+        book_id SERIAL PRIMARY KEY,
+        title VARCHAR(100) NOT NULL,
+        author VARCHAR(100) NOT NULL,
+        rating DECIMAL(3,2),
+        description TEXT,
+        genre_id INT NOT NULL,
+        image_url VARCHAR(255),
+        active BOOLEAN,
+        CONSTRAINT fk_genre_id FOREIGN KEY(genre_id) REFERENCES genres(genre_id)
+      );
+      CREATE TABLE users (
+          user_id SERIAL PRIMARY KEY,
+          name VARCHAR(100) NOT NULL,
+          email VARCHAR(100) UNIQUE NOT NULL,
+          username VARCHAR(50) UNIQUE NOT NULL,
+          password VARCHAR(50) NOT NULL,
+          status VARCHAR(20)
+      );
+      CREATE TABLE comments (
+          comment_id SERIAL PRIMARY KEY,
+          user_id INT NOT NULL,
+          book_id INT NOT NULL,
+          comment TEXT NOT NULL,
+          rating DECIMAL(3,2),
+          CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
+          CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES books(book_id)
+      );
+      CREATE TABLE genres (
+          genre_id SERIAL PRIMARY KEY,
+          name VARCHAR(100) UNIQUE NOT NULL
+      );
+    `);
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error;
   }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+    // await createInitial Books/Users
+  } catch (error) {
+    console.log("Error during rebuildDB");
+    throw error;
+  }
+}

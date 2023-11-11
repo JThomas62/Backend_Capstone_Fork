bda60ceccffa2d0c25349ff2ecc0cc60ebafc6d9
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2023-11-11 13:14:05 -0500

    Update seed.js
    Reordered tables for creation and deletion

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,5 +1,7 @@
 const {
     client,
+    createUser,
+    createGenre,
 
 } = require("./index");
 
@@ -9,10 +11,10 @@ async function dropTables() {
   
       // have to make sure to drop in correct order
       await client.query(`
-        DROP TABLE IF EXISTS books;
-        DROP TABLE IF EXISTS users;
         DROP TABLE IF EXISTS comments;
+        DROP TABLE IF EXISTS books;
         DROP TABLE IF EXISTS genres;
+        DROP TABLE IF EXISTS users;
       `);
   
       console.log("Finished dropping tables!");
@@ -28,6 +30,18 @@ async function createTables() {
     console.log("Starting to build tables...");
 
     await client.query(`
+      CREATE TABLE genres (
+          genre_id SERIAL PRIMARY KEY,
+          name VARCHAR(100) UNIQUE NOT NULL
+      );
+      CREATE TABLE users (
+        user_id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        password VARCHAR(50) NOT NULL,
+        status VARCHAR(20)
+    );
       CREATE TABLE books (
         book_id SERIAL PRIMARY KEY,
         title VARCHAR(100) NOT NULL,
@@ -39,14 +53,6 @@ async function createTables() {
         active BOOLEAN,
         CONSTRAINT fk_genre_id FOREIGN KEY(genre_id) REFERENCES genres(genre_id)
       );
-      CREATE TABLE users (
-          user_id SERIAL PRIMARY KEY,
-          name VARCHAR(100) NOT NULL,
-          email VARCHAR(100) UNIQUE NOT NULL,
-          username VARCHAR(50) UNIQUE NOT NULL,
-          password VARCHAR(50) NOT NULL,
-          status VARCHAR(20)
-      );
       CREATE TABLE comments (
           comment_id SERIAL PRIMARY KEY,
           user_id INT NOT NULL,
@@ -55,10 +61,6 @@ async function createTables() {
           rating DECIMAL(3,2),
           CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
           CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES books(book_id)
-      );
-      CREATE TABLE genres (
-          genre_id SERIAL PRIMARY KEY,
-          name VARCHAR(100) UNIQUE NOT NULL
       );
     `);
     console.log("Finished building tables!");
